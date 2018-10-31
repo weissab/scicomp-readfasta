4962a2e5684b04530f393bd2d9874df0102fc904
weissab <andrea.weiss7@gmail.com>
2018-10-31 09:55:45 -0400

    Revert "Checks whether filename is on command line"
    This reverts commit b096d72dd56dd808f4bacf3f7bbdf53faf6b3ee0.

--- read_fasta_v1.py
+++ read_fasta_v1.py
@@ -2,15 +2,14 @@ import sys
 
 def read_fasta(filename):
     sequence = ''
-    f = open (filename)
+    f = open ('ae.fa')
     for line in f:
         line = line.strip()
         if not'>' in line:
             sequence = sequence + line
     f.close() 
     return sequence
-if len(sys.argv) <  2:
-	print ('Usage: ', sys.argv[0], '<sequence.fa>'
-	exit(1) 
-print (read_fasta(sys.argv[1]))
-
+print (read_fasta('sys.argv[1]'))
+#  python read_fasta_v2.py
+# sys.arg
+#  ['read_fasta_v2.py', 'ae.fa']
